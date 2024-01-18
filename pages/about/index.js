e0129307248050d52bfe325442956cbafc1ac88f
Victor Olatunji <victoluolatunji@gmail.com>
2024-01-18 04:10:09 +0100

    setup/layouts

---
+++ pages/about/index.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout';
+
+export default function About() {
+  return (
+    <Layout title={`About DJ Events`}>
+      <div>
+        <h2>About</h2>
+      </div>
+    </Layout>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import Layout from '../components/Layout';
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Layout>
+        <h1>Home</h1>
+      </Layout>
+    </>
+  );
 }
