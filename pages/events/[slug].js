8b2b6732723568bdcda4304d7e1e9dbc906f6a88
Victor Olatunji <victoluolatunji@gmail.com>
2024-02-02 11:01:26 +0100

    getstaticprops

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,6 +1,5 @@
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
-
 export default function Event({ evt }) {
   return (
     <Layout>
@@ -10,14 +9,19 @@ export default function Event({ evt }) {
   );
 }
 
-export async function getServerSideProps({ query: { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`);
-
-  const events = await res.json();
-
-  return {
-    props: {
-      evt: events[0],
-    },
-  };
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/event/${slug}`);
+  const event = await res.json();
 }
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+
+//   const events = await res.json();
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   };
+// }
